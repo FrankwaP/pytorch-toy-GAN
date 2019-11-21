529602254379bea8fe2bb66feb9c398512f0e2b6
frankwa <francois.plessier@gmail.com>
2019-11-21 14:18:00 +0100

    Correcting the silly mistake for the Generator optimizer. Now Generator and Discriminator loop until the satisfy a condition on their loss. If the loss is high enough, it works like the original GAN (1 loop for each).

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # pytorch-toy-GAN
+
+Toy model to learn how to mess with a GAN model (see: https://arxiv.org/abs/1406.2661)
+The training data are generated using a normal distribution, meaning:
+    - The generator is trained to generate fake normal distributions from noise (uniform distribution).
+    - The discriminator is trained to detect if normal distributions are real or fake.
+
+Both distributions -- normal and noise/uniform -- are generated on the fly.
+It's nothing special regarding the noise distribution.
+But regarding the normal distributions, it has 2 advantages:
+    - this simulate infinite data
+    - this makes the code much shorter
--- networks.py
+++ networks.py
@@ -23,7 +23,6 @@ import torch
 import torch.nn as nn
 import seaborn as sns
 import matplotlib.pyplot as plt
-from scipy.stats import  shapiro
 # optimizer to play with
 from torch.optim import Adam
 
@@ -37,11 +36,33 @@ else:
 #%% Functions to generate the data
 
 def generate_real_data(batch_size, sample_size):
-    mu, sig = 300., 1.
+    mu, sig = 2., 0.5
     return mu + sig*torch.randn((batch_size, sample_size)).to(device)
 
 def generate_noise(batch_size, noise_size):
     return torch.rand((batch_size, noise_size)).to(device)
+
+#%% Fcuntion for distribution plotting
+def results_to_seaborn_plot():
+    def best_sample(x, y):
+        return x[torch.argmax(y), :].tolist(), torch.max(y)
+    def worst_sample(x, y):
+        return x[torch.argmin(y), :].tolist(), torch.min(y)
+    
+    xy_dic = {'real': (x_real, y_real_pred), 'fake1': (x_fake_comp, y_fake_pred_comp),
+               'fake2': (x_fake2, y_fake_pred2)}
+
+    sample_list = []
+    label_list = []
+    for ref, xy in xy_dic.items():
+        for i, func in enumerate((best_sample, worst_sample)):
+            sample, score = func(xy[0], xy[1])
+            sample_list += sample
+            label = ref+'-'+str(i)+': %.2f' % score
+            label_list += [label]*len(sample)
+    fig, ax = plt.subplots(figsize=(16,9))
+    sns.violinplot(ax=ax, x=sample_list, y=label_list).set_title('epoch '+str(epoch+1))
+    plt.pause(0.001)
 
 
 #%% Discriminator and Generator
@@ -53,93 +74,105 @@ class sort_layer(nn.Module):
 def get_discriminator(n_input, n_hidden, n_output=1):
     return nn.Sequential(
         sort_layer(),
-        # nn.BatchNorm1d(n_input),   <== it was very stupid since when training D there are batches with 100% real and batches with 100% fake
-        nn.Linear(n_input, n_hidden), nn.LeakyReLU(),
-        nn.Linear(n_hidden, n_output), nn.Sigmoid())
+        # nn.Dropout(0.9),
+        nn.Linear(n_input, n_output), 
+        nn.Sigmoid())
 
 def get_generator(n_input, n_hidden, n_output):
     return nn.Sequential(
-        nn.Linear(n_input, n_hidden), nn.Tanh(),
+        nn.Linear(n_input, n_hidden),
+        nn.Tanh(),
         nn.Linear(n_hidden, n_output))
+
+
+# We use the BCE loss for both Discriminator and Generator
+# This doesn't correspond to equation 1 and algorithm 1, regarding the Generator
+# (watch out: the equations use logs and not -logs like the BCE)
+# However: "Rather than training G to minimize log(1 - D(G(z))) we can train G to maximize logD(G(z)).
+# This objective function results in the same fixed point of the dynamics of G and D
+# but provides much stronger gradients early in learning."
+# Using the BCE loss for the generator does this.
+loss = nn.BCELoss()
+
 
 #%% Training the GAN: batch level
 
 batch_size = 10000
-noise_size = 10
+noise_size = 100
 sample_size = 1000
-n_epoch = 10000
-record_step = 100
+n_epoch = 100
+plot_step = 1
 
-D = get_discriminator(sample_size, 2**6, 1)
-D.cuda()
-D_optim = Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
+D = get_discriminator(sample_size, 2**3, 1)
+D.to(device)
 
-
-G = get_generator(noise_size, 2**6, sample_size)
-G.cuda()
-G_optim = Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
-
-loss = nn.BCELoss()
-
+G = get_generator(noise_size, 2**4, sample_size)
+G.to(device)
 
 y_real = torch.ones((batch_size, 1)).to(device)
 y_fake = 1-y_real
 
+loss_limit = 0.11  # correspond to a 90% success mean: -ln(0.9) == 0.11
 
-#%%
-n_loop, n_D_loop, n_G_loop = 0, 0, 0
-G_samples, G_loops, G_titles = [], [], []
+x_fake_comp = None
+y_fake_pred_comp = None
 
-G_samples, G_epochs, G_titles = [], [], []
 for epoch in range(n_epoch):
-    for _ in range(20):
-        ###
-        # "Update the discriminator by ascending its stochastic gradient"
-        # fake data should NOT be predicted as real (negative loss for "ascending")
+    # Training the Discriminator
+    D_optim = Adam(D.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    loss_chk = 2*loss_limit
+    while loss_chk > loss_limit:
+        # Discriminator loss on fake data (should be fake)
         z_fake = generate_noise(batch_size, noise_size)
-        x_fake = G(z_fake) #.detach()
-        y_fake_pred = D(x_fake)
-        D_fake_loss = -loss(y_fake_pred, y_real)
-        # real data should NOT be predicted as fake (negative loss for "ascending")
+        x_fake1 = G(z_fake)
+        # if old_x_fake1 is None:
+        y_fake_pred1 = D(x_fake1)
+        D_fake_loss = loss(y_fake_pred1, y_fake)
+        # else:            
+        #     y_fake_pred1 = D(torch.cat((x_fake1, old_x_fake1)))
+        #     D_fake_loss = loss(y_fake_pred1, torch.cat((y_fake, y_fake)))
+        # Discriminator loss on real data (should be real)
         x_real = generate_real_data(batch_size, sample_size)
         y_real_pred = D(x_real)
-        D_real_loss = -loss(y_real_pred, y_fake)
+        D_real_loss = loss(y_real_pred, y_real)
         ## combining losses
         D_loss = 0.5*(D_real_loss + D_fake_loss)
+        loss_chk = D_loss.data
+        # fitting Discriminator parameter
         D.zero_grad()
         D_loss.backward()
         D_optim.step()
         ###
-        print('Epoch [%d/%d], D_real_loss: %5.2f, D_fake_loss: %5.2f, D_loss: %5.2f, G_loss: %5.2f'
-              % (epoch+1, n_epoch, D_real_loss.data, D_fake_loss.data, D_loss.data, 0))
-
-    for _ in range(50):
-        ###
-        # "Update the generator by descending its stochastic gradient"
-        # fake data should be predicted as real
+        print('Epoch [%d/%d], D_real_loss: %5.2f, D_fake_loss: %5.2f, D_loss: %5.2f, G_loss: %5s'
+              % (epoch+1, n_epoch, D_real_loss.data, D_fake_loss.data, D_loss.data, ''))
+    old_x_fake1 = x_fake1.detach()
+    # Training the Generator
+    loss_chk = 2*loss_limit
+    G_optim = Adam(G.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    while loss_chk > loss_limit:
+        # Generator loss on fake data (should be real)
         z_fake = generate_noise(batch_size, noise_size)
-        x_fake = G(z_fake)
-        y_pred = D(x_fake)
-        G_loss = loss(y_pred, y_real)
-        #
+        x_fake2 = G(z_fake)
+        y_fake_pred2 = D(x_fake2)
+        G_loss = loss(y_fake_pred2, y_real)
+        loss_chk = G_loss.data
+        # fitting Generator parameter
         G.zero_grad()
         G_loss.backward()
         G_optim.step()
         ###
-        print('Epoch [%d/%d], D_real_loss: %5.2f, D_fake_loss: %5.2f, D_loss: %5.2f, G_loss: %5.2f'
-              % (epoch+1, n_epoch, D_real_loss.data, D_fake_loss.data, D_loss.data, G_loss.data))
+        print('Epoch [%d/%d], D_real_loss: %5s, D_fake_loss: %5s, D_loss: %5s, G_loss: %5.2f'
+              % (epoch+1, n_epoch, '', '', '', G_loss.data))
+    if ((epoch+1) % plot_step) == 0:
+        if x_fake_comp is None:
+            x_fake_comp = x_fake1
+            y_fake_pred_comp = y_fake_pred1
+        results_to_seaborn_plot()
+        x_fake_comp = x_fake2
+        y_fake_pred_comp = y_fake_pred2
 
-    # if ((loop+1) % record_step) == 0:
-    G_loop = ['%03d' % (epoch+1)]*sample_size
-    G_sample_ = x_fake[torch.argmax(y_pred), :]
-    G_sample = G_sample_.tolist()
-    m, s = G_sample_.mean(), G_sample_.std()
-    t, p = shapiro(G_sample)  # H0: it is from a normal distribution
-    G_title = 'mean: %.2f, std: %.2f, p-value: %.2e' % (m, s, p)
-    sns.violinplot(x=G_loop, y=G_sample).set_title(G_title)
-    plt.pause(0.001)
-    G_loops += G_loop
-    G_samples += G_sample
-    G_titles += G_title
 
-sns.violinplot(x=G_loops[-10*sample_size:], y=G_samples[-10*sample_size:])
+#%%
+tmp1 = y_fake_pred2.tolist()  # predicted values in list (for Excel)
+tmploss = nn.BCELoss(reduction='none')  # redefining a loss giving the whole BCEloss tensor
+tmp2 = tmploss(y_fake_pred2, y_real).tolist()  # BCEloss values in list (for Exel)
